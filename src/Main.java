b2d895a4f50d165a7f26b0c179392112e14e7b49
Ирина <mi.irene97@gmail.com>
2022-09-24 19:42:01 +0300

    + вторая часть ДЗ

--- src/Main.java
+++ src/Main.java
@@ -34,6 +34,49 @@ public class Main {
             population = population + (population/1000 * birthsPer1000) - (population/1000 * deathsPer1000);
             System.out.println("Год " + i + ", численность населения составляет " + population + " человек.");
         }
+        // ДЗ Часть 2
+        System.out.println("ДЗ Часть 2");
+        // Задание 1
+        System.out.println("Задание 1");
+        total = 15_000;
+        monthlySavings = 15_000;
+        for (i = 1; total <= 12_000_000; i++) {
+            total = total + total/100 * 7;
+            total = total + monthlySavings;
+            System.out.println("Месяц " + i + ", сумма накоплений составляет " + total + " рублей.");
+        }
+        // Задание 2
+        System.out.println("Задание 2");
+        total = 15_000;
+        monthlySavings = 15_000;
+        for (i = 1; total <= 12_000_000; i++) {
+            total = total + total / 100 * 7;
+            total = total + monthlySavings;
+            if (i % 6 == 0) {
+                System.out.println("Месяц " + i + ", сумма накоплений составляет " + total + " рублей.");
+            }
+        }
+        // Задание 3
+        System.out.println("Задание 3");
+        total = 15_000;
+        monthlySavings = 15_000;
+        int monthsIn9Years = 12 * 9;
+        for (i = 1; i <= monthsIn9Years; i++) {
+            total = total + total / 100 * 7;
+            total = total + monthlySavings;
+            if (i % 6 == 0) {
+                System.out.println("Месяц " + i + ", сумма накоплений составляет " + total + " рублей.");
+            }
+        }
+        // Задание 4
+        System.out.println("Задание 4");
+        int firstFriday = 2;
+        int date;
+        for (date = 1; date <= 31; date++) {
+            if (date == firstFriday || (date - firstFriday) % 7 == 0) {
+                System.out.println("Сегодня пятница, " + date + " число. Необходимо подготовить отчёт.");
+            }
+        }
 
     }
 }
